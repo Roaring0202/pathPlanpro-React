115c8b1b7ee36943316e195e9c10c218fd912058
edwardpower116 <oliveranderson116ok@outlook.com>
2024-05-29 17:41:31 +0530

    added hero-section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './Hero.css'
+import heroppp from '../../assets/images/heroppp.jpg'
 const Hero = () => {
   return (
+    <>
     <div className='hero-section'>
+      <span className="hero-content">Unlock your Potential with<br/> <span className='title'>PathPlanPro</span></span>
+      <span className='title-description'>Discover the step-by-step roadmaps to master new technologies and skills.</span>
+      <button className='explore-button'>Explore</button>
+      <button className='signup-button'>Sign Up</button>
+      <img className='hero-image' src={heroppp} alt="" />
     </div>
+    </>
   )
 }
 
