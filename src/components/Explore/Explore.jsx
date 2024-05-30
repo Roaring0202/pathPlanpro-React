1e731234c750fdce8d549cb297e537efb0c7759f
edwardpower116 <oliveranderson116ok@outlook.com>
2024-05-30 23:25:09 +0530

    refactor explore section

--- src/components/Explore/Explore.jsx
+++ src/components/Explore/Explore.jsx
@@ -8,12 +8,12 @@ const Explore = () => {
             <div className="splitter">
                 <div className="roles">
                     <p>Discover your role and step into a position that truly fits you. Uncover the perfect match for your skills and ambitions.</p>
-                    <button className='role-button'>Roles</button>
+                    <button className='role-button'>ROLES</button>
                 </div>
                 <hr style={{ border: '3px solid black',height:'400px',borderRadius:'20px' }} />
                 <div className="skills">
                     <p>Sharpen your skills and stay ahead in your field. Invest in continuous learning to enhance your expertise and achieve excellence.</p>
-                    <button className='skill-button'>Skills</button>
+                    <button className='skill-button'>SKILLS</button>
                 </div>
             </div>
     </div>
